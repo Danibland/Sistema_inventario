dc44a75cc7df1f66eb66aa0c84c89cb3ae9495d8
Daniel Blandon <danielblandon_01@hotmail.com>
2025-02-28 11:53:51 -0500

    creacion del proyecto

---
+++ vistas/modulos/cabezote.php
@@ -0,0 +1,24 @@
+ <header class="main-header">
+ 	
+	<!--=====================================
+	LOGOTIPO
+	======================================-->
+	<a href="inicio" class="logo">
+		
+		<!-- logo mini -->
+		<span class="logo-mini">
+			
+			<img src="vistas/img/plantilla/icono-blanco.png" class="img-responsive" style="padding:10px">
+
+		</span>
+
+		<!-- logo normal -->
+
+		<span class="logo-lg">
+			
+			<img src="vistas/img/plantilla/logo-blanco-lineal.png" class="img-responsive" style="padding:10px 0px">
+
+		</span>
+
+	</a>
+</header> 
